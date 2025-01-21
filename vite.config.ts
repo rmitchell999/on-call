f4148be35e721737615eae4b6f022acd0e84ba2f
rmitchell999 <rmitchell0402@gmail.com>
2025-01-21 12:24:22 +0000

    Update Login

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
-import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,5 +11,13 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
-})
+  },
+  optimizeDeps: {
+    include: ['@aws-amplify/ui-vue'],
+  },
+  build: {
+    rollupOptions: {
+      external: ['@aws-amplify/ui-vue'],
+    },
+  },
+});
