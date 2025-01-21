ab4af522aa99e31e7134e447fe6271237aca6e64
rmitchell999 <rmitchell0402@gmail.com>
2025-01-21 13:41:16 +0000

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,6 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { Authenticator } from '@aws-amplify/ui-react';
-import { Amplify } from 'aws-amplify';
-import outputs from '@/amplify_outputs.json';
-import '@aws-amplify/ui-react/styles.css';
+import { Authenticator } from '@aws-amplify/ui-vue';
 import OnCallApplication from './components/OnCallApplication.vue';
 
 export default defineComponent({
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,7 @@ import "./assets/main.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import { Amplify } from "aws-amplify";
-import outputs from "./amplify_outputs.json";
-import { defineAuth } from "@aws-amplify/backend"
+import outputs from "amplify_outputs.json";
 
 Amplify.configure(outputs);
 
